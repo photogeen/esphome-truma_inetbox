e2a15829d596db7fcd5e8720167f3b25df648fc4
photogeen <eugen@photogeen.com>
2025-05-27 16:26:04 +0200

    Update TrumaHeaterSelect.cpp
    vento mode added

--- components/truma_inetbox/select/TrumaHeaterSelect.cpp
+++ components/truma_inetbox/select/TrumaHeaterSelect.cpp
@@ -32,44 +32,66 @@ void TrumaHeaterSelect::setup() {
             break;
         }
         break;
-
-      case TRUMA_SELECT_TYPE::HEATER_ENERGY_MIX:
-        switch (status->energy_mix_a) {
-          case EnergyMix::ENERGY_MIX_GAS:
-            this->publish_state(this->at((size_t) TRUMA_SELECT_TYPE_HEATER_ENERGY_MIX::GAS).value());
+      case TRUMA_SELECT_TYPE::VENT_MODE:
+        switch (status->vent_mode) {
+          case VentMode::VENT_MODE_1:
+            this->publish_state(this->at((size_t) TRUMA_SELECT_TYPE_VENT_MODE::VENT_1).value());
             break;
-          case EnergyMix::ENERGY_MIX_MIX:
-            switch (status->el_power_level_a) {
-              case ElectricPowerLevel::ELECTRIC_POWER_LEVEL_900:
-                this->publish_state(this->at((size_t) TRUMA_SELECT_TYPE_HEATER_ENERGY_MIX::MIX_1).value());
-                break;
-              case ElectricPowerLevel::ELECTRIC_POWER_LEVEL_1800:
-                this->publish_state(this->at((size_t) TRUMA_SELECT_TYPE_HEATER_ENERGY_MIX::MIX_2).value());
-                break;
-              default:
-                this->publish_state(this->at((size_t) TRUMA_SELECT_TYPE_HEATER_ENERGY_MIX::GAS).value());
-                break;
-            }
+          case VentMode::VENT_MODE_2:
+            this->publish_state(this->at((size_t) TRUMA_SELECT_TYPE_VENT_MODE::VENT_2).value());
             break;
-          case EnergyMix::ENERGY_MIX_ELECTRICITY:
-            switch (status->el_power_level_a) {
-              case ElectricPowerLevel::ELECTRIC_POWER_LEVEL_900:
-                this->publish_state(this->at((size_t) TRUMA_SELECT_TYPE_HEATER_ENERGY_MIX::ELECTRIC_1).value());
-                break;
-              case ElectricPowerLevel::ELECTRIC_POWER_LEVEL_1800:
-                this->publish_state(this->at((size_t) TRUMA_SELECT_TYPE_HEATER_ENERGY_MIX::ELECTRIC_2).value());
-                break;
-              default:
-                this->publish_state(this->at((size_t) TRUMA_SELECT_TYPE_HEATER_ENERGY_MIX::GAS).value());
-                break;
-            }
+          case VentMode::VENT_MODE_3:
+            this->publish_state(this->at((size_t) TRUMA_SELECT_TYPE_VENT_MODE::VENT_3).value());
+            break;
+          case VentMode::VENT_MODE_4:
+            this->publish_state(this->at((size_t) TRUMA_SELECT_TYPE_VENT_MODE::VENT_4).value());
+            break;
+          case VentMode::VENT_MODE_5:
+            this->publish_state(this->at((size_t) TRUMA_SELECT_TYPE_VENT_MODE::VENT_5).value());
+            break;
+          case VentMode::VENT_MODE_6:
+            this->publish_state(this->at((size_t) TRUMA_SELECT_TYPE_VENT_MODE::VENT_6).value());
+            break;
+          case VentMode::VENT_MODE_7:
+            this->publish_state(this->at((size_t) TRUMA_SELECT_TYPE_VENT_MODE::VENT_7).value());
+            break;
+          case VentMode::VENT_MODE_8:
+            this->publish_state(this->at((size_t) TRUMA_SELECT_TYPE_VENT_MODE::VENT_8).value());
+            break;
+          case VentMode::VENT_MODE_9:
+            this->publish_state(this->at((size_t) TRUMA_SELECT_TYPE_VENT_MODE::VENT_9).value());
+            break;
+          case VentMode::VENT_MODE_10:
+            this->publish_state(this->at((size_t) TRUMA_SELECT_TYPE_VENT_MODE::VENT_10).value());
+            break;
+          case VentMode::VENT_MODE_ECO:
+            this->publish_state(this->at((size_t) TRUMA_SELECT_TYPE_VENT_MODE::ECO).value());
+            break;
+          case VentMode::VENT_MODE_HIGH:
+            this->publish_state(this->at((size_t) TRUMA_SELECT_TYPE_VENT_MODE::HIGH).value());
             break;
           default:
-            this->publish_state(this->at((size_t) TRUMA_SELECT_TYPE_HEATER_ENERGY_MIX::GAS).value());
+            this->publish_state(this->at((size_t) TRUMA_SELECT_TYPE_VENT_MODE::OFF).value());
             break;
         }
         break;
-
+      case TRUMA_SELECT_TYPE::HEATER_ENERGY_MIX:
+        if (status->energy_mix_a == EnergyMix::ENERGY_MIX_GAS) {
+          this->publish_state(this->at((size_t) TRUMA_SELECT_TYPE_HEATER_ENERGY_MIX::GAS).value());
+        } else if (status->energy_mix_a == EnergyMix::ENERGY_MIX_MIX) {
+          if (status->el_power_level_a == ElectricPowerLevel::ELECTRIC_POWER_LEVEL_900) {
+            this->publish_state(this->at((size_t) TRUMA_SELECT_TYPE_HEATER_ENERGY_MIX::MIX_1).value());
+          } else if (status->el_power_level_a == ElectricPowerLevel::ELECTRIC_POWER_LEVEL_1800) {
+            this->publish_state(this->at((size_t) TRUMA_SELECT_TYPE_HEATER_ENERGY_MIX::MIX_2).value());
+          }
+        } else if (status->energy_mix_a == EnergyMix::ENERGY_MIX_ELECTRICITY) {
+          if (status->el_power_level_a == ElectricPowerLevel::ELECTRIC_POWER_LEVEL_900) {
+            this->publish_state(this->at((size_t) TRUMA_SELECT_TYPE_HEATER_ENERGY_MIX::ELECTRIC_1).value());
+          } else if (status->el_power_level_a == ElectricPowerLevel::ELECTRIC_POWER_LEVEL_1800) {
+            this->publish_state(this->at((size_t) TRUMA_SELECT_TYPE_HEATER_ENERGY_MIX::ELECTRIC_2).value());
+          }
+        }
+        break;
       default:
         break;
     }
@@ -92,7 +114,6 @@ void TrumaHeaterSelect::control(const std::string &value) {
     case TRUMA_SELECT_TYPE::HEATER_FAN_MODE:
       switch ((TRUMA_SELECT_TYPE_HEATER_FAN_MODE) index.value()) {
         case TRUMA_SELECT_TYPE_HEATER_FAN_MODE::ECO:
-          // case TRUMA_SELECT_TYPE_HEATER_FAN_MODE::VARIO_HEAT_NIGHT:
           if (heater_device == TRUMA_DEVICE::CPPLUS_VARIO) {
             this->parent_->get_heater()->action_heater_room(static_cast<u_int8_t>(temp),
                                                          HeatingMode::HEATING_MODE_VARIO_HEAT_NIGHT);
@@ -101,7 +122,6 @@ void TrumaHeaterSelect::control(const std::string &value) {
           }
           break;
         case TRUMA_SELECT_TYPE_HEATER_FAN_MODE::COMBI_HIGH:
-          // case TRUMA_SELECT_TYPE_HEATER_FAN_MODE::VARIO_HEAT_AUTO:
           if (heater_device == TRUMA_DEVICE::CPPLUS_VARIO) {
             this->parent_->get_heater()->action_heater_room(static_cast<u_int8_t>(temp),
                                                          HeatingMode::HEATING_MODE_VARIO_HEAT_AUTO);
@@ -117,33 +137,75 @@ void TrumaHeaterSelect::control(const std::string &value) {
           break;
       }
       break;
-
+    case TRUMA_SELECT_TYPE::VENT_MODE:
+      switch ((TRUMA_SELECT_TYPE_VENT_MODE) index.value()) {
+        case TRUMA_SELECT_TYPE_VENT_MODE::VENT_1:
+          this->parent_->get_heater()->action_heater_vent_mode(VentMode::VENT_MODE_1);
+          break;
+        case TRUMA_SELECT_TYPE_VENT_MODE::VENT_2:
+          this->parent_->get_heater()->action_heater_vent_mode(VentMode::VENT_MODE_2);
+          break;
+        case TRUMA_SELECT_TYPE_VENT_MODE::VENT_3:
+          this->parent_->get_heater()->action_heater_vent_mode(VentMode::VENT_MODE_3);
+          break;
+        case TRUMA_SELECT_TYPE_VENT_MODE::VENT_4:
+          this->parent_->get_heater()->action_heater_vent_mode(VentMode::VENT_MODE_4);
+          break;
+        case TRUMA_SELECT_TYPE_VENT_MODE::VENT_5:
+          this->parent_->get_heater()->action_heater_vent_mode(VentMode::VENT_MODE_5);
+          break;
+        case TRUMA_SELECT_TYPE_VENT_MODE::VENT_6:
+          this->parent_->get_heater()->action_heater_vent_mode(VentMode::VENT_MODE_6);
+          break;
+        case TRUMA_SELECT_TYPE_VENT_MODE::VENT_7:
+          this->parent_->get_heater()->action_heater_vent_mode(VentMode::VENT_MODE_7);
+          break;
+        case TRUMA_SELECT_TYPE_VENT_MODE::VENT_8:
+          this->parent_->get_heater()->action_heater_vent_mode(VentMode::VENT_MODE_8);
+          break;
+        case TRUMA_SELECT_TYPE_VENT_MODE::VENT_9:
+          this->parent_->get_heater()->action_heater_vent_mode(VentMode::VENT_MODE_9);
+          break;
+        case TRUMA_SELECT_TYPE_VENT_MODE::VENT_10:
+          this->parent_->get_heater()->action_heater_vent_mode(VentMode::VENT_MODE_10);
+          break;
+        case TRUMA_SELECT_TYPE_VENT_MODE::ECO:
+          this->parent_->get_heater()->action_heater_vent_mode(VentMode::VENT_MODE_ECO);
+          break;
+        case TRUMA_SELECT_TYPE_VENT_MODE::HIGH:
+          this->parent_->get_heater()->action_heater_vent_mode(VentMode::VENT_MODE_HIGH);
+          break;
+        default:
+          this->parent_->get_heater()->action_heater_vent_mode(VentMode::VENT_MODE_OFF);
+          break;
+      }
+      break;
     case TRUMA_SELECT_TYPE::HEATER_ENERGY_MIX:
       switch ((TRUMA_SELECT_TYPE_HEATER_ENERGY_MIX) index.value()) {
         case TRUMA_SELECT_TYPE_HEATER_ENERGY_MIX::GAS:
-          this->parent_->get_heater()->action_heater_energy_mix(EnergyMix::ENERGY_MIX_GAS);
+          this->parent_->get_heater()->action_heater_energy_mix(EnergyMix::ENERGY_MIX_GAS,
+                                                             ElectricPowerLevel::ELECTRIC_POWER_LEVEL_0);
           break;
         case TRUMA_SELECT_TYPE_HEATER_ENERGY_MIX::MIX_1:
           this->parent_->get_heater()->action_heater_energy_mix(EnergyMix::ENERGY_MIX_MIX,
-                                                                ElectricPowerLevel::ELECTRIC_POWER_LEVEL_900);
+                                                             ElectricPowerLevel::ELECTRIC_POWER_LEVEL_900);
           break;
         case TRUMA_SELECT_TYPE_HEATER_ENERGY_MIX::MIX_2:
           this->parent_->get_heater()->action_heater_energy_mix(EnergyMix::ENERGY_MIX_MIX,
-                                                                ElectricPowerLevel::ELECTRIC_POWER_LEVEL_1800);
+                                                             ElectricPowerLevel::ELECTRIC_POWER_LEVEL_1800);
           break;
         case TRUMA_SELECT_TYPE_HEATER_ENERGY_MIX::ELECTRIC_1:
           this->parent_->get_heater()->action_heater_energy_mix(EnergyMix::ENERGY_MIX_ELECTRICITY,
-                                                                ElectricPowerLevel::ELECTRIC_POWER_LEVEL_900);
+                                                             ElectricPowerLevel::ELECTRIC_POWER_LEVEL_900);
           break;
         case TRUMA_SELECT_TYPE_HEATER_ENERGY_MIX::ELECTRIC_2:
           this->parent_->get_heater()->action_heater_energy_mix(EnergyMix::ENERGY_MIX_ELECTRICITY,
-                                                                ElectricPowerLevel::ELECTRIC_POWER_LEVEL_1800);
+                                                             ElectricPowerLevel::ELECTRIC_POWER_LEVEL_1800);
           break;
         default:
           break;
       }
       break;
-
     default:
       break;
   }
@@ -152,11 +214,6 @@ void TrumaHeaterSelect::control(const std::string &value) {
 void TrumaHeaterSelect::dump_config() {
   LOG_SELECT("", "Truma Heater Select", this);
   ESP_LOGCONFIG(TAG, "  Type '%s'", enum_to_c_str(this->type_));
-  ESP_LOGCONFIG(TAG, "  Options are:");
-  // auto options = this->traits.get_options();
-  // for (auto i = 0; i < this->mappings_.size(); i++) {
-  //   ESP_LOGCONFIG(TAG, "    %i: %s", this->mappings_.at(i), options.at(i).c_str());
-  // }
 }
 }  // namespace truma_inetbox
 }  // namespace esphome
